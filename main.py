9c07baf870eca89a0a98f880321ff3fe5fa20f7d
LauGds <137927575+LauGds@users.noreply.github.com>
2023-09-15 11:19:22 -0700

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    # Write your code here
+    valleys = 0
+    altitude = 0
+    
+    for step in path:
+        if step == "D":
+            if altitude == 0:
+                valleys += 1
+            altitude -= 1
+        elif step =="U":
+            altitude += 1
+    return valleys
